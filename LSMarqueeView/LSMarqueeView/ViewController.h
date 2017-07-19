d922ba646aa22d734529126482581da04836beba
LSRain <315192042@qq.com>
2017-07-19 14:12:27 +0800

    project init

---
+++ LSMarqueeView/LSMarqueeView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LSMarqueeView
+//
+//  Created by WangBiao on 2017/7/19.
+//  Copyright © 2017年 LSRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LSMarqueeView/LSMarqueeView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LSMarqueeView
+//
+//  Created by WangBiao on 2017/7/19.
+//  Copyright © 2017年 LSRain. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
